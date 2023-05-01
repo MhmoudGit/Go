2acc7d8465ed2b99c08f5e275f7df97fc56a6cf7
MhmoudGit <mahmoud.a.a.y18@gmail.com>
2023-05-01 13:05:26 +0300

    go

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloHandler(res http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(res, "hello world")
+}
 
 func main() {
-	fmt.Println("Hello World")
+	http.HandleFunc("/", helloHandler)
+	fmt.Println("server is running on port: 8000")
+	if err := http.ListenAndServe(":8000", nil); err != nil {
+		log.Fatal(err)
+	}
 }
